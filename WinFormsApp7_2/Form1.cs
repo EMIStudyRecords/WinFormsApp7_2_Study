63e90951411a6310de4743b5b4c6cdb0f6e7067a
鈴木 映美 <yon.ncity@gmail.com>
2023-12-08 09:29:24 +0900

    電話帳に名前を登録

--- WinFormsApp7_2/Form1.cs
+++ WinFormsApp7_2/Form1.cs
@@ -2,9 +2,18 @@ namespace WinFormsApp7_2
 {
     public partial class Form1 : Form
     {
+        Dictionary<string, string> phoneBook;
+
         public Form1()
         {
             InitializeComponent();
+
+            // 電話帳に名前を登録する
+            this.phoneBook = new Dictionary<string, string>();
+            this.phoneBook.Add("山田一郎","090-1234-5678");
+            this.phoneBook.Add("山田二郎", "090-2345-6789");
+            this.phoneBook.Add("山田三郎", "090-3456-7890");
+            this.phoneBook.Add("山田史郎", "090-4567-8901");
         }
 
         private void NameSelected(object sender, EventArgs e)
