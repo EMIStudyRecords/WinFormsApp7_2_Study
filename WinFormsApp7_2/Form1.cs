24aa30c58631ac6f5a2d290af4ea1e9c774a1dfd
鈴木 映美 <yon.ncity@gmail.com>
2023-12-08 10:57:25 +0900

    テキストファイルからデータを読み込む

--- WinFormsApp7_2/Form1.cs
+++ WinFormsApp7_2/Form1.cs
@@ -9,12 +9,17 @@ namespace WinFormsApp7_2
             InitializeComponent();
 
             // 電話帳に名前を登録する
+            
             this.phoneBook = new Dictionary<string, string>();
+            /*
             this.phoneBook.Add("山田一郎","090-1234-5678");
             this.phoneBook.Add("山田二郎", "090-2345-6789");
             this.phoneBook.Add("山田三郎", "090-3456-7890");
             this.phoneBook.Add("山田史郎", "090-4567-8901");
+            */
 
+            ReadFormFile();
+            
             // リストに名前を表示させる
             foreach (KeyValuePair<string,string> data in phoneBook)
             {
@@ -22,6 +27,20 @@ namespace WinFormsApp7_2
             }
         }
 
+        // テキストファイルの読み込み
+        private void ReadFormFile()
+        {
+            using (System.IO.StreamReader file =
+                new System.IO.StreamReader(@"C:\Users\鈴木　映美\source\repos\WinFormsApp7_2\WinFormsApp7_2\data.txt"))
+            {
+                while (!file.EndOfStream)
+                {
+                    string line = file.ReadLine();
+                    string[] data = line.Split(',');
+                    this.phoneBook.Add(data[0], data[1]);
+                }
+            }
+        }
         // 選択した名前に対応する電話番号を表示する
         private void NameSelected(object sender, EventArgs e)
         {
